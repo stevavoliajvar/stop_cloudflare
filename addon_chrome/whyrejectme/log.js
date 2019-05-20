4daec001868ebb056ff141c2f00a8f49adc8154a
cloudflaredns <cloud@flare.dns>
2019-05-20 14:41:38 +0000

    Upload files to 'addon_chrome/whyrejectme'

---
+++ addon_chrome/whyrejectme/log.js
@@ -0,0 +1,22 @@
+function newrow(w) {
+	let tt = document.getElementsByTagName('tbody')[0];
+	let tr = tt.insertRow(tt.rows.length);
+	tr.innerHTML = w;
+}
+
+newrow('<tr><td><b>FQDN</b></td><td><b>Server</b></td><td><b>Code</b></td><td><b>Where</b></td><td><b>When</b></td></tr>');
+
+chrome.runtime.sendMessage('get', function (g) {
+	for (let k in g) {
+		newrow('<tr><td>' + k + '</td><td>' + g[k][1] + '</td><td>' + g[k][2] + '</td><td><a href="' + g[k][0] + '" rel="noreferrer" target="_blank">Link</a></td><td>' + g[k][3] + '</td></tr>');
+		document.getElementById('export').innerHTML += k + "\n";
+	}
+});
+
+document.body.style.display = 'block';
+
+document.getElementById('clear').addEventListener('click', () => {
+	chrome.runtime.sendMessage('clear', function () {
+		location.reload(true);
+	});
+});
