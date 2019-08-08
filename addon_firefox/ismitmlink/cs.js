fc8af9e153865c99cd88f268ebd953b1094f4a38
yourfriend <yourfriend@noreply.codeberg.org>
2019-08-08 03:21:52 +0200

    Upload files to 'addon_firefox/ismitmlink'

--- addon_firefox/ismitmlink/cs.js
+++ addon_firefox/ismitmlink/cs.js
@@ -5,36 +5,36 @@ if (document.body && !['searxes.nmqnkngye4ct7bgss4bmv5ca3wpa55yugvxen5kz2bbq67lw
 		return s.sheet;
 	})();
 	if (cs) {
-		cs.insertRule("a[data-mitm]{cursor:not-allowed !important;text-decoration-line:line-through !important;text-decoration-color:red !important;text-decoration-style:double !important}", 0);
-		cs.insertRule("a[data-mitm]::before{content:'[\\26A0]';font-weight:bold !important;color:red !important;display:inline-block !important}", 1);
-		cs.insertRule("a[data-mitm]:hover::before{content:'[\\26A1]'}", 2);
-		cs.insertRule("a[data-mitm]:hover{color:red !important}", 3);
-		cs.insertRule("img[data-mitm]{cursor:not-allowed !important;border:2px red dotted !important}", 4);
-		cs.insertRule("img[data-mitm]:hover{transform:scaleY(-1);filter:sepia(70%)}", 5);
+		cs.insertRule("a[data-mitm=y]{cursor:not-allowed !important;text-decoration-line:line-through !important;text-decoration-color:red !important;text-decoration-style:double !important}", 0);
+		cs.insertRule("a[data-mitm=y]::before{content:'[\\26A0]';font-weight:bold !important;color:red !important;display:inline-block !important}", 1);
+		cs.insertRule("a[data-mitm=y]:hover::before{content:'[\\26A1]'}", 2);
+		cs.insertRule("a[data-mitm=y]:hover{color:red !important}", 3);
+		cs.insertRule("img[data-mitm=y]{cursor:not-allowed !important;border:2px red dotted !important}", 4);
+		cs.insertRule("img[data-mitm=y]:hover{filter:sepia(20%)}", 5);
 	}
-	let asked = ['searxes.nmqnkngye4ct7bgss4bmv5ca3wpa55yugvxen5kz2bbq67lwy6ps54yd.onion', 'searxes.eu.org', 'api.searxes.eu.org', 'addons.mozilla.org', 'addons.thunderbird.net', 'web.archive.org'];
-	document.querySelectorAll("a[href^='http://']:not([data-mitm]),a[href^='https://']:not([data-mitm]),a[href^='//']:not([data-mitm])").forEach(a => {
-		let aF = (new URL(a.href)).hostname;
+
+	let asked = ['', 'searxes.nmqnkngye4ct7bgss4bmv5ca3wpa55yugvxen5kz2bbq67lwy6ps54yd.onion', 'searxes.eu.org', 'api.searxes.eu.org', 'addons.mozilla.org', 'addons.thunderbird.net', 'web.archive.org'];
+	document.querySelectorAll('a[href]:not([data-mitm]),img[src]:not([data-mitm])').forEach(a => {
+		let aF = (a.tagName == 'A' ? (new URL(a.href)).hostname : (new URL(a.src)).hostname) || '';
 		if (!/^(.*)\.(danwin1210\.me|onion|i2p|invalid|test|local|localhost|([0-9]{1,3})|bbs|chan|cyb|dyn|geek|gopher|indy|libre|neo|null|o|oss|oz|parody|pirate|bit|lib|coin|emc|bazar|fur)$/.test(aF) && !asked.includes(aF)) {
 			asked.push(aF);
 			browser.runtime.sendMessage(aF);
 		}
 	});
-	document.querySelectorAll("img[src^='http://']:not([data-mitm]),img[src^='https://']:not([data-mitm]),img[src^='//']:not([data-mitm])").forEach(a => {
-		let aF = (new URL(a.src)).hostname;
-		if (!/^(.*)\.(danwin1210\.me|onion|i2p|invalid|test|local|localhost|([0-9]{1,3})|bbs|chan|cyb|dyn|geek|gopher|indy|libre|neo|null|o|oss|oz|parody|pirate|bit|lib|coin|emc|bazar|fur)$/.test(aF) && !asked.includes(aF)) {
-			asked.push(aF);
-			browser.runtime.sendMessage(aF);
-		}
-	});
+
 	browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		if (request.length == 2) {
-			if (request[1]) {
-				document.querySelectorAll("a[href^='http://" + request[0] + "/']:not([data-mitm]),a[href^='https://" + request[0] + "/']:not([data-mitm]),a[href^='//" + request[0] + "/']:not([data-mitm]),img[src^='http://" + request[0] + "/']:not([data-mitm]),img[src^='https://" + request[0] + "/']:not([data-mitm]),img[src^='//" + request[0] + "/']:not([data-mitm])").forEach(a => {
-					a.dataset.mitm = 1;
-					a.title = 'MITM!';
-				});
-			}
+			document.querySelectorAll('a[href]:not([data-mitm]),img[src]:not([data-mitm])').forEach(a => {
+				let aF = (a.tagName == 'A' ? (new URL(a.href)).hostname : (new URL(a.src)).hostname) || '';
+				if (aF == request[0]) {
+					if (request[1]) {
+						a.dataset.mitm = 'y';
+						a.title = 'MITM!';
+					} else {
+						a.dataset.mitm = 'n';
+					}
+				}
+			});
 		}
 		sendResponse(null);
 	});
