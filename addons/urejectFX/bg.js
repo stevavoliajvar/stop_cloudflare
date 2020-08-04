299faf36f979204088409d521e4e3b82522447ab
Lana Gulden <0313d585463a4190948b5856e96c5b74ae842199@cloudflare.com>
2020-08-04 04:38:13 +0200

    bg.js

--- addons/urejectFX/bg.js
+++ addons/urejectFX/bg.js
@@ -2,7 +2,7 @@ let history = {};
 browser.webRequest.onResponseStarted.addListener(i => {
 	if ([403, 406, 410, 429, 451, 462].includes(i.statusCode)) {
 		let iFQDN = (new URL(i.url)).hostname;
-		if (['searxes.eu.org', 'ansero.eu.org', 'ss.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion', 'searxes.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion', 'ansero.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion'].includes(iFQDN)) {
+		if (['crimeflare.eu.org', 'ansero.eu.org', 'crimeflare.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion', 'ansero.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion'].includes(iFQDN)) {
 			return;
 		}
 		let iServer = '?';
