cc058d936f41ecea6f54cdb903404266858d38a2
Renee Taormina <7c0df5934ced2eeeb3d721a2f28c9f3c5ad573ab@cloudflare.com>
2020-11-10 08:41:37 +0100

    cs.js

--- addons/ismitmlinkFX/cs.js
+++ addons/ismitmlinkFX/cs.js
@@ -1,4 +1,4 @@
-if (document.body && !['crimeflare.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion', 'crimeflare.eu.org', 'api.crimeflare.eu.org', 'ansero.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion', 'ansero.eu.org'].includes(location.hostname)) {
+if (document.body && !['crimeflare.eu.org', 'api.crimeflare.eu.org', 'ansero.eu.org'].includes(location.hostname) && !location.hostname.endsWith('.onion')) {
 	let cs = (function () {
 		let s = document.createElement('style');
 		document.head.appendChild(s);
@@ -13,7 +13,7 @@ if (document.body && !['crimeflare.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxc
 		cs.insertRule("img[data-mitm=y]:hover{filter:sepia(20%)}", 5);
 	}
 	browser.storage.local.get(['ign1', 'ign2', 'obs', 'dbg', 'alt']).then(g => {
-		let asked = ['', 'crimeflare.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion', 'crimeflare.eu.org', 'api.crimeflare.eu.org', 'ansero.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxcyroopbhsuqd.onion', 'ansero.eu.org', 'addons.mozilla.org', 'addons.thunderbird.net', 'web.archive.org', 't.co'];
+		let asked = ['', 'cfe.nnpaefp7pkadbxxkhz2agtbv2a4g5sgo2fbmv3i7czaua354334uqqad.onion', 'crimeflare.eu.org', 'api.crimeflare.eu.org', 'ansero.nnpaefp7pkadbxxkhz2agtbv2a4g5sgo2fbmv3i7czaua354334uqqad.onion', 'ansero.eu.org', 'addons.mozilla.org', 'addons.thunderbird.net', 'web.archive.org', 't.co'];
 		if (g.ign1 == 'y') {
 			asked.push(location.hostname);
 		}
@@ -28,7 +28,7 @@ if (document.body && !['crimeflare.wodferndripvpe6ib4uz4rtngrnzichnirgn7t5x64gxc
 					a.dataset.mitm = aF;
 					if (!asked.includes(aF)) {
 						asked.push(aF);
-						if (!/^(.*)\.(danwin1210\.me|wikipedia\.org|onion|i2p|invalid|test|local|localhost|([0-9]{1,3})|bbs|chan|cyb|dyn|geek|gopher|indy|libre|neo|null|o|oss|oz|parody|pirate|bit|lib|coin|emc|bazar|fur)$/.test(aF) && aF.length >= 4) {
+						if (!/^(.*)\.(wikipedia\.org|onion|i2p|invalid|test|local|localhost|([0-9]{1,3})|bbs|chan|cyb|dyn|geek|gopher|indy|libre|neo|null|o|oss|oz|parody|pirate|bit|lib|coin|emc|bazar|fur)$/.test(aF) && aF.length >= 4) {
 							unknown.push(aF);
 						}
 					}
